e75b110d2a2cfb2d810c846448b8789479869f63
RaulGarcia2 <74016283+RaulGarcia2@users.noreply.github.com>
2021-02-09 19:07:11 +0100

    Add files via upload

---
+++ csv-psql.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import argparse
+import libreria
+
+# Gestionamos los argumentos del script
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--fichero", help="Fichero a procesar")
+parser.add_argument("-b", "--basedatos", help="Base de datos de trabajo" )
+parser.add_argument("-m", "--host", help="Máquina dónde está la base de datos (por defecto localhost)" )
+parser.add_argument("-p", "--puerto", help="Puerto de postgresql (por defecto 5432)" )
+parser.add_argument("-u", "--user", help="Usuario de postgresql" )
+parser.add_argument("-c", "--password", help="Contraseña del usuario de postgresql" )
+parser.add_argument("-d", help="Se borra la tabla si esta existe", action="store_true" )
+
+args = parser.parse_args()
+
+if not args.fichero:
+    print('Necesitamos un fichero')
+    quit()
+
+print("El fichero es " + args.fichero)
+fichero=args.fichero.strip()
+
+if not args.basedatos:
+    print("Necesitamos una base de datos")
+    quit()
+
+basedatos=args.basedatos.strip()
+
+puerto=5432
+host=''
+usuario=''
+password=''
+
+if args.host:
+    host = args.host
+
+if args.puerto:
+    puerto = args.puerto
+
+if args.user:
+    usuario= args.user.strip()
+
+if args.password:
+    password=args.password.strip()
+
+# Comprobación del fichero de entrada de datos
+try:
+    f = open(fichero)
+except:
+    print('Error al abrir el fichero ' + fichero)
+    quit()
+
+n=0
+
+#definimos objeto base y conectamos con la base de datos 
+base=libreria.Base(basedatos, 'localhost', puerto, usuario, password)
+b=base.conecta()
+if b[:5] == 'Error':
+    print(b)
+    quit()
+
+#nombre de la tabla igual que el nombre del fichero
+tabla=fichero.split('.')[0]
+
+#navegamos por el fichero
+for linea in f:
+
+    linea=linea[:len(linea)-1]
+    if n == 0: 
+        # Primera linea del fichero con los nombres y tio de campos
+        campos=linea.split(",")
+        if base.existeTabla(tabla) > 0:
+            if args.d:
+                t=base.borraTabla(tabla)
+                print(t)
+            else:
+                print('tabla ' + tabla + ' ya existe')
+                quit()
+
+        t=base.creaTabla(tabla, campos)
+        print(t)
+        if t[:5]=="Error":
+            quit()
+    else:
+        # Añadimos registros a la tabla
+        valores=linea.split(",")       
+        t=base.anadeRegistro(tabla, campos, valores)
+        if t != None:
+            print ('Error añadir registro: ' + t)
+            
+    n+=1
+
+print('Registros añadidos')
+f.close()
---
+++ libreria.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import pg
+
+class Base:
+    nombre=''
+    host=''
+    puerto=5432
+    usuario=''
+    pasword=''
+
+    def __init__(self, nombre, host, puerto=5432, usuario='', password=''):
+        self.nombre=nombre
+        self.host=host
+        self.puerto=puerto
+        if usuario:
+            self.usuario = usuario
+        if password:
+            self.password = password
+
+    def conecta(self):
+        #self.db = pg.DB()
+        
+        try:
+            if self.usuario != '':
+                self.db = pg.connect(dbname=self.nombre,  host=self.host, port=self.puerto, passwd=self.password, user=self.usuario)   
+            else:
+                self.db = pg.connect(self.nombre, self.host, self.puerto)
+        except TypeError:
+            return 'Error BD argumentos'
+        except SyntaxError:
+            return 'Error BD argumentos duplicados'
+        except:
+            return 'Error desconocido conectanco a' + self.nombre
+        return 'Conectado'
+
+    def borraTabla(self, tabla):
+        sql = 'DROP TABLE ' + tabla + ';'
+        try:
+            res=self.db.query(sql)
+        except:
+            return 'Error borrando tabla'
+        return 'Tabla borrada'
+
+    def creaTabla(self, nombre, campos):
+        sql='CREATE TABLE ' + nombre + ' ("'
+        for x in campos:
+            campo=x.split(':')[0]
+            tipo=x.split(':')[1]
+            sql += campo + '" ' + tipo + ', "'
+        
+        sql=sql[:len(sql)-3]
+        sql += ");"
+        try:
+            res=self.db.query(sql)
+            return 'Creada tabla ' + nombre
+        except:
+            if self.db.error[-15:-1] == 'already exists':
+                self.borraTabla(nombre)
+                res=self.db.query(sql)
+            return 'Error creando tabla '+ nombre 
+        
+    def anadeRegistro(self, tabla, campos, valores):
+        sql='INSERT INTO ' + tabla + ' ("'
+        for a in campos:
+            sql += a.split(":")[0] + '", "'
+        
+        sql=sql[:len(sql)-3] + ') VALUES ('
+        n=0
+        for a in valores:
+            tipo = campos[n].split(':')[1]
+            if tipo[0:7] == 'varchar':
+                tipo='varchar'
+
+            if a=='':
+                a='Null'
+            sql += case(tipo) + a + case(tipo) + ', ' 
+            n += 1
+        sql = sql[:len(sql)-2] + ');'
+        try:
+            res = self.db.query(sql)
+        except:
+            print(sql)
+            return 'Error añadiendo datos'
+        
+    def existeTabla(self, tabla):
+        sql="SELECT table_name FROM information_schema.columns WHERE table_name='" + tabla + "'"
+        res = self.db.query(sql)
+        return res.ntuples()
+        
+def case(argumento):
+    dicc = {
+        'varchar' : "'",
+        'char' : "'",
+        'time' : "'",
+        'timestamp' : "'",
+        'date' : "'",
+        'integer' : '',
+        'double' : '',
+        'float' : ''
+    }
+    return dicc.get(argumento, 'Null')
